88a09c1ddcd71e49804d942b7736e0614df8a9d8
SuperSheep18 <nothingwillgowrong@gmail.com>
2015-02-28 12:00:45 -0700

    Small improvement to connect.py

--- Connect.py
+++ Connect.py
@@ -1,8 +1,15 @@
 import ClientHandler
 import SceneManager
+import glob
 
 path = "C:\Users\w\Documents\Models\osg-data-master\glider.osg"
 name = "somethingyouwant"
+
+paths = glob.glob("C:\Users\Aedan\Desktop\Homework Folder\CMPUT 302\DICOM Models\COU\*.dcm")
+for i in paths:
+	i = i[66:]
+	print i
+
 
 local_IP = "25.79.169.119"
 OptiTrack_IP = "25.79.169.119"
