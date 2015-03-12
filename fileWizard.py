0fd222588d06f5ee3741fa6f787ec3298ba860e5
Victoria Bobey <vbobey@ualberta.ca>
2015-03-12 11:32:43 -0600

    Converted chooseTest to a widget. Created a widget for allowing the user to select a file.

---
+++ fileWizard.py
@@ -0,0 +1,44 @@
+
+"""
+A widget for opening modules. Allows the user to select the module
+they want to open in their file system with a GUI.
+"""
+import sys
+from PyQt4 import QtGui
+
+class FileWizard(QtGui.QWidget):
+    
+    def __init__(self):
+        super(FileWizard, self).__init__()
+        # Create a push button that links to a file dialog         
+        self.fileButton = QtGui.QPushButton("Open a Module")
+        self.fileButton.clicked.connect(self.openFile)        
+        
+        # TODO: Using this as a random variable that the filepath is 
+        # being set to for now.
+        self.filePath = ''
+        
+        # Layout for the widget
+        mainLayout = QtGui.QVBoxLayout()
+        mainLayout.addWidget(self.fileButton)
+        self.setLayout(mainLayout)
+        self.show()
+
+    # Uses file dialog to get path to module, returns a string
+    def openFile(self):
+        f = QtGui.QFileDialog.getOpenFileName(self, "Open Data File", "")
+        # convert filepath to a string
+        f = str(f)
+        print(f)                      
+        
+        # Not sure what to do with filepath, so im setting it to a variable for now
+        self.filePath = f;
+        
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = FileWizard()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
