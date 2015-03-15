64688ca8b4fb37742ea38792310d20b28c49acbf
jaeyoon1 <jaeyoon1@ualberta.ca>
2015-03-15 14:54:23 -0600

    Refactor polyDict, polyModel has its own class now. made util class for common functions (only has euclid method for now) methods for absolute position and rotation (not tested)

--- ModelFunctionTest.py
+++ ModelFunctionTest.py
@@ -6,8 +6,91 @@ import glob
 import time
 import math
 
-def euclid(c1, c2):
-	return ( math.sqrt( (c2[0] - c1[0])**2 + (c2[1] - c1[1])**2 + (c2[2] - c1[2])**2 ) )
+#TODO: put classes in different files
+
+class util():
+	@staticmethod
+	def euclid(c1, c2):
+		return ( math.sqrt( (c2[0] - c1[0])**2 + (c2[1] - c1[1])**2 + (c2[2] - c1[2])**2 ) )
+
+class polyModel():
+	def __init__(self,path,name):
+		self.model = None
+		self.name = name
+		self.path = path
+		if not(SceneManager.doesModelExist(name)):
+			SceneManager.loadPolygonModel(path,name)
+			self.model = SceneManager.getModel(name)
+			SceneManager.addNodeToScene(name,"mainView")
+		else:
+			self.model = SceneManager.getModel(name)
+
+	#TODO: test to see if destructor actually works
+	def __del__(self):
+		SceneManager.deleteModel(self.getName())
+		return
+
+	def getName(self):
+		return self.name
+
+	def getPath(self):
+		return self.path
+
+	def getModel(self):
+		if self.model is None:
+			print "Model is returning None"
+		return self.model
+
+	#TODO: test what it returns when there are no rigidbody attached
+	#also not working...?
+	def getRigidBody(self):
+		return self.getModel().getRigidBody()
+
+	def attachRigidBody(self,rigid_body):
+		self.getModel().attachRigidBody(rigid_body)
+		return 
+
+	#Set/Get Translation of model in X,Y,Z
+	def getPositionOffset(self):
+		return self.getModel().getPositionOffset()
+
+	#gets absolute position with the rigidbody taken into account
+	#the whole tuple/operator mess adds the values for both tuples for every index.
+	#otherwise you end up with (a,b) + (d,e) = (a,b,c,d) instead of (a+c,b+d)
+	def getAbsPositionOffset(self):
+		return tuple(map(operator.add, self.getPositionOffset(), self.getRigidBody().getPosition()))
+		
+	def setPositionOffset(self,x,y,z):
+		try:
+			self.getModel().setPositionOffset(x,y,z)
+		except:
+			return 1
+		return 0
+
+	#Set/Get rotation of model in X,Y,Z,W
+	def getRotationOffset(self):
+		return self.getModel().getRotationOffset()
+
+	def getAbsRotationOffset(self):
+		return tuple(map(operator.add, self.getRotationOffset(), self.getRigidBody().getRotation()))
+	
+	def setRotationOffset(self,x,y,z,w):
+		try:
+			self.getModel().setRotationOffset(x,y,z,w)
+		except:
+			return 1
+		return 0
+
+	#Set/Get Scale of model in X,Y,Z
+	def getScale(self):
+		return self.getModel().getScale()
+
+	def setScale(self,x,y,z):
+		try:
+			self.getModel().setScale(x,y,z)
+		except:
+			return 1
+		return 0
 
 class polyModelDict():
 	def __init__(self):
@@ -19,13 +102,11 @@ class polyModelDict():
 	def getSize(self):
 		return len(self.polyModelDict)
 
-
 	#Returns the entire dictionary 
 	def getDict(self):
 		return self.polyModelDict
 
-
-	#Returns a reference to the polygon model
+	#Returns a reference to the polyModel object
 	def getPolygonModel(name):
 		try:
 			return self.polyModelDict[name]
@@ -33,27 +114,14 @@ class polyModelDict():
 			return None
 		return None
 
-
 	#Load a polygon model
 	def loadPolygonModel(self,path,name):
-		try:
-			if not(SceneManager.doesModelExist(name)):
-				SceneManager.loadPolygonModel(path,name)
-				self.polyModelDict[name] = SceneManager.getModel(name)
-			else:
-				print("loadPolygonModel - a model with specified name already exists")
-		except:
-			print("loadPolygonModel - something happened")
-			print("path: "+path)
-			print("name: "+name)
-			return 1
-		return 0
+		self.polyModelDict[name] = polyModel(path,name)
+		return
 	
-
 	#Deletes a model
 	def deletePolygonModel(self,name):
 		try:
-			SceneManager.deleteModel(name)
 			self.polyModelDict.pop(name)
 		except:
 			print("deletePolygonModel - something happened")
@@ -61,58 +129,6 @@ class polyModelDict():
 			return 1
 		return 0
 
-
-	#Set/Get Translation of model in X,Y,Z
-	def getPositionOffset(self,name):
-		return self.polyModelDict[name].getPositionOffset()
-
-	def setPositionOffset(self,name,position):
-		try:	
-			if len(position) == 3:
-				self.polyModelDict[name].setPositionOffset(position[0],position[1],position[2])
-			else:
-				print "invalid position input"
-				return 1
-		except:
-			return 1
-		return 0
-
-	#Set/Get rotation of model in X,Y,Z,W
-	def getRotationOffset(self,name):
-		return self.polyModelDict[name].getRotationOffset()
-
-	def setRotationOffset(self,name,quat):
-		try:
-			if len(quat) == 4:
-				self.polyModelDict[name].setRotationOffset(quat[0],quat[1],quat[2],quat[3])
-			else:
-				print"invalid rotation input"
-				return 1
-		except:
-			return 1
-		return 0
-
-	#Set/Get Scale of model in X,Y,Z
-	def getScale(self,name):
-		return self.polyModelDict[name].getScale()
-
-	def setScale(self,name,scale):
-		try:
-			if len(scale) == 3:
-				self.polyModelDict[name].setScale(scale[0],scale[1],scale[2])
-			else:
-				print"invalid scale input"
-			return 1
-		except:
-			return 1
-		return 0
-
-	def attachRigidBody(self,name,rigid_body):
-		try:
-			self.polyModelDict[name].attachRigidBody[rigid_body]
-		except:
-			return 1
-		return 0
 
 
 print "---------------------------------------"
@@ -135,49 +151,49 @@ name3 = "Cow3"
 #modelist = list([0]*len(paths))
 #test loading multiple models
 
-#path = 'C:\Program Files\ProjectDR\Model\osg-data-master\cessna.osg'
-path = 'C:\Users\Aedan\Desktop\Homework Folder\CMPUT 302\osg-data\cow.osg'
+path = 'C:\Users\w\Documents\Models\osg-data-master\cow.osg'
+#path = 'C:\Program Files\ProjectDR\Model\osg-data-master\cow.osg'
+#path = 'C:\Users\Aedan\Desktop\Homework Folder\CMPUT 302\osg-data\cow.osg'
 
-polyDict = polyModelDict()
 
-polyDict.loadPolygonModel(path,name)
-polyDict.loadPolygonModel(path,name2)
+model1 = polyModel(path,name)
+model2 = polyModel(path,name2)
 
 """
-print "position: "+str(polyDict.getPositionOffset(name))
-print "rotation: "+str(polyDict.getRotationOffset(name))
-print "scale: "+str(polyDict.getScale(name))
-polyDict.setPositionOffset(name,(1.0,2.0,4.0))
-polyDict.setRotationOffset(name,(1.0,2.0,4.0,8.0))
-polyDict.setScale(name,(1.0,2.0,4.0))
-print "position: "+str(polyDict.getPositionOffset(name))
-print "rotation: "+str(polyDict.getRotationOffset(name))
-print "scale: "+str(polyDict.getScale(name))
+print "position: "+str(model1.getPositionOffset())
+print "rotation: "+str(model1.getRotationOffset())
+print "scale: "+str(model1.getScale())
+model1.setPositionOffset(0.2,0.5,0.2)
+model1.setRotationOffset(1.0,2.0,4.0,1.0)
+model1.setScale(name,(0.1,0.1,0.1)
+print "position: "+str(model1.getPositionOffset())
+print "rotation: "+str(model1.getRotationOffset())
+print "scale: "+str(model1.getScale())
 """
-
 startX = 10.0
 startY = 0.0
 startZ = 10.0
 
-polyDict.setPositionOffset(name, (startX, startY, startZ))
-polyDict.setPositionOffset(name2, (-startX, startY, startZ))
-polyDict.setScale(name,(.1,.1,.1))
-polyDict.setScale(name2,(.1,.1,.1))
+model1.setPositionOffset(startX, startY, startZ)
+model2.setPositionOffset(-startX, startY, startZ)
+model1.setScale(.1,.1,.1)
+model2.setScale(.1,.1,.1)
 
-a = polyDict.getPositionOffset(name)
-b = polyDict.getPositionOffset(name2)
-while ( euclid(a,b) > .8):
+a = model1.getPositionOffset()
+b = model2.getPositionOffset()
+while ( util.euclid(a,b) > .8):
 	time.sleep(.125)
 	startX -= 0.125
 	startZ -= 0.125
-	polyDict.setPositionOffset(name, (startX, startY, startZ))
-	polyDict.setPositionOffset(name2, (-startX, startY, -startZ))
-	a = polyDict.getPositionOffset(name)
-	b = polyDict.getPositionOffset(name2)
+	model1.setPositionOffset(startX, startY, startZ)
+	model2.setPositionOffset(-startX, startY, -startZ)
+	a = model1.getPositionOffset()
+	b = model2.getPositionOffset()
 
-polyDict.loadPolygonModel(path, name3)
-polyDict.setScale(name3,(.1,.1,.1))
-polyDict.setPositionOffset(name3, (0.0, 0.0, .5))
+model3 = polyModel(path,name3)
+model3.setScale(.1,.1,.1)
+model3.setPositionOffset(0.0, 0.0,.5)
+
 
 #write to file
 #something.encode("utf-8")
