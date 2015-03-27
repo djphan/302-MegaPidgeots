531f746a677814167c7f563435c8bdf1cb5b5ae5
Victoria Bobey <vbobey@ualberta.ca>
2015-03-27 16:11:57 -0600

    File wizard allows the user to choose between full or back, sends choice to ProjectDr

--- fileWizard.py
+++ fileWizard.py
@@ -1,46 +1,55 @@
-
 """
 A widget for opening modules. Allows the user to select the module
 they want to open in their file system with a GUI.
 """
 import sys
+import os
 from PyQt4 import QtGui
-import SceneManager
+# import SceneManager
 
 class FileWizard(QtGui.QWidget):
-    
-    def __init__(self):
-        super(FileWizard, self).__init__()
-        # Create a push button that links to a file dialog         
-        self.fileButton = QtGui.QPushButton("Open a Module")
-        self.fileButton.clicked.connect(self.openFile)        
-        
-        # TODO: Using this as a random variable that the filepath is 
-        # being set to for now.
-        self.filePath = ''
-        
-        # Layout for the widget
-        mainLayout = QtGui.QVBoxLayout()
-        mainLayout.addWidget(self.fileButton)
-        self.setLayout(mainLayout)
-        self.show()
+	def __init__(self):
+		super(FileWizard, self).__init__()
+		# Create a push button that links to a file dialog
+		self.fileButton = QtGui.QPushButton("Open a Module")
+		self.fileButton.clicked.connect(self.openFile)
 
-    # Uses file dialog to get path to module, returns a string
-    def openFile(self):
-        f = QtGui.QFileDialog.getOpenFileName(self, "Open Module", "")
-        # convert filepath to a string
-        f = str(f)
-        print(f)
-        SceneManager.loadPolygonModel(f,"whateveryoufeellikeman")                      
-        
-        # Not sure what to do with filepath, so im setting it to a variable for now
-        self.filePath = f;
-        
+		# Layout for the widget
+		mainLayout = QtGui.QVBoxLayout()
+		mainLayout.addWidget(self.fileButton)
+		self.setLayout(mainLayout)
+		self.show()
+
+	# Uses file dialog to get path to module, returns a string
+	def openFile(self):
+		path = QtGui.QFileDialog.getOpenFileName(self, "Open Module", "")
+
+		# convert path to a string
+		path = str(path)
+		# get filename from path
+		(dir, file) = os.path.split(path)
+
+		# based on the file, pick the name for the model
+		if (file == 'Skeleton_FULL.OSGB'):
+			model = 'skeleton1'
+		elif (file == 'Skeleton_Back.OSGB'):
+			model = 'skeleton2'
+		else:
+			title = "Invalid File"
+			error = "Please choose Skeleton_FULL or Skeleton_Back."
+			QtGui.QMessageBox.critical(None, title, error, QtGui.QMessageBox.Close)
+			return
+
+		# Send the model to scene manager
+		SceneManager.loadPolygonModel(path, model)
+		return
+
+
 def main():
-    app = QtGui.QApplication(sys.argv)
-    ex = FileWizard()
-    sys.exit(app.exec_())
+	app = QtGui.QApplication(sys.argv)
+	ex = FileWizard()
+	sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
-    main()
+	main()
