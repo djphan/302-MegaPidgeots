60973b244bd152a871174485af05cd40250b1587
Dan <djphan@ualberta.ca>
2015-04-15 09:40:54 -0600

    Update util.py

--- util.py
+++ util.py
@@ -26,20 +26,20 @@ class util():
 	@staticmethod
 	def isOver(pos1,pos2,w,l): 
 		#pos1 and pos2 is a length 3 tuple, where pos1 is the acting button with width and height w,l and pos2 is the thing pressing the button
-		X1 = pos1[0] - w/2
-		X2 = pos1[0] + w/2
-		Z1 = pos1[2] - l/2
-		Z2 = pos1[2] + l/2
+		X1 = pos1[0] - w/2.0
+		X2 = pos1[0] + w/2.0
+		Z1 = pos1[2] - l/2.0
+		Z2 = pos1[2] + l/2.0
 		return ((pos2[0]>=X1 and pos2[0]<=X2) and (pos2[2]>=Z1 and pos2[2]<=Z2))
 
 	#pos1 and pos2 is a length 3 tuple, where pos1 is the acting button with width and height w,l and pos2 is the thing pressing the button
 	#pressed dist is the minimum distance between the two on the y axis in order to be considered pressed
 	@staticmethod
 	def isPressed(pos1,pos2,w,l,pressdist):
-		X1 = pos1[0] - w/2
-		X2 = pos1[0] + w/2
-		Z1 = pos1[2] - l/2
-		Z2 = pos1[2] + l/2
+		X1 = pos1[0] - w/2.0
+		X2 = pos1[0] + w/2.0
+		Z1 = pos1[2] - l/2.0
+		Z2 = pos1[2] + l/2.0
 
 		return (((pos2[0]>=X1 and pos2[0]<=X2) and (pos2[2]>=Z1 and pos2[2]<=Z2)) and (abs(pos1[1] - pos2[1])<pressdist))
 	
